2158e6eb63838a88f53955540086829085fdf35d
Dharmendra Kumar <dharm117x@gmail.com>
2022-06-08 20:08:33 +0530

    Update sorting order.

--- CoreJava/src/com/java/stream/StreamGroupApi.java
+++ CoreJava/src/com/java/stream/StreamGroupApi.java
@@ -8,30 +8,22 @@ import java.util.stream.Collectors;
 
 public class StreamGroupApi {
 	public static void main(String[] args) {
-		SaleData data1 = new SaleData("Jan", 122.0);
-		SaleData data2 = new SaleData("Jan", 102.0);
-		SaleData data4 = new SaleData("Dec", 522.99);
-		SaleData data5 = new SaleData("Feb", 422.99);
-		SaleData data3 = new SaleData("Mar", 422.99);
-		
 		
 		List<SaleData> datas = new ArrayList<>();
-		datas.add(data1);
-		datas.add(data2);
-		datas.add(data3);
-		datas.add(data4);
-		datas.add(data5);
+		datas.add(new SaleData("Jan", 100.0));
+		datas.add(new SaleData("Jan", 100.0));
+		datas.add(new SaleData("Feb", 300.0));
+		datas.add(new SaleData("Mar", 400.0));
+		datas.add(new SaleData("Dec", 500.0));
+		datas.add(new SaleData("Dec", 500.0));
 		
-		System.out.println("Deafult:: "+datas);
 		List<SaleData> sorted = datas.stream().sorted(Comparator.comparing(SaleData::getAmt, Comparator.reverseOrder())).collect(Collectors.toList());
 		System.out.println("Sorted by Amt-> "+sorted);
 		
 		Map<String, Double> collect = datas.stream()
 				.collect(Collectors.groupingBy(SaleData::getMonth, Collectors.summingDouble(SaleData::getAmt)));
 
-		System.out.println(collect);
-
-		List<SaleData> collect2 = collect.entrySet().stream().sorted((m1,m2)->m2.getKey().compareTo(m1.getKey()))
+		List<SaleData> collect2 = collect.entrySet().stream().sorted((m1,m2)->m2.getValue().compareTo(m1.getValue()))
 				.map(e -> new SaleData(e.getKey(), e.getValue())).collect(Collectors.toList());
 		
 		System.out.println(collect2);
